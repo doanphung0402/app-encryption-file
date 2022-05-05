e17c8a3c629a24292e261479a64cfdbc2d9c6794
DoanPhung <doanphung0402@gmail.com>
2022-05-05 14:47:21 +0700

    update activity_home line 83

--- app/src/main/java/com/example/project3/HomeActivity.java
+++ app/src/main/java/com/example/project3/HomeActivity.java
@@ -27,16 +27,6 @@ import java.util.List;
 import java.util.Optional;
 
 public class HomeActivity extends AppCompatActivity {
-    ImageView imgview = (ImageView) findViewById(R.id.img_openfile);
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (imgview.getVisibility() == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true);
-
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +47,7 @@ public class HomeActivity extends AppCompatActivity {
     public void  onActivityResult(int requestcode, int resultCode, Intent data){
         ListView lv = (ListView) findViewById(R.id.list_view_file);
         ArrayList<FileChooserInfo> listFile ;
-        ArrayList<String> fileChooser = new ArrayList<>();
+        ArrayList<Uri> fileChooser = new ArrayList<>();
         FileChooserAdapter fileChooserAdapter ;
         Context context = getApplicationContext();
         super.onActivityResult(requestcode,resultCode,data);
@@ -69,45 +59,37 @@ public class HomeActivity extends AppCompatActivity {
              if(null != data.getClipData()){
                  for (int i =0 ; i<data.getClipData().getItemCount(); i++){
                      Uri uri = data.getClipData().getItemAt(i).getUri();
-                     fileChooser.add(uri.getPath());
+                     fileChooser.add(uri);
                  }
+
+             }else{
+                  Uri uri = data.getData();
+                  fileChooser.add(uri);
              }
-              Uri uri = data.getData();
-              String nameFile =getFileName(uri,getApplicationContext());
-              String urlFile = uri.getPath();
-              String extentionFile  = getExtensionByStringHandling(nameFile).get();
-
-              int nameImg ;
-             switch(extentionFile) {
-                 case "pdf":
-                     nameImg = R.drawable.dpf_image ;
-                     break;
-                 case "docx" :
-                     nameImg = R.drawable.word_image ;
-                     break;
-                 case "mp4" :
-                     nameImg = R.drawable.mp4_image ;break;
-                 case "jpg" :
-                     nameImg = R.drawable.jpg_image ;break;
-                 case "mp3" :
-                     nameImg = R.drawable.mp3_icon ;
-                     break;
-                 case "txt" :
-                     nameImg = R.drawable.text_image ;
-                     break;
-
-                 default:
-                     nameImg = R.drawable.unknown_image ;
+             for (Uri c : fileChooser){
+                 Toast.makeText(this, "file choose: "+c , Toast.LENGTH_SHORT).show();
+                 System.out.println("file : "+c);
              }
-              System.out.println("name file "+ nameFile+",extention file : "+extentionFile);
-              listFile =new ArrayList<>();
-              listFile.add(new FileChooserInfo(nameFile,urlFile,nameImg));
-              fileChooserAdapter = new FileChooserAdapter(this,R.layout.file_chooser_info_item_activity,listFile);
-              lv.setAdapter(fileChooserAdapter);
+//              Uri uri = data.getData();
+//              String nameFile =getFileName(uri,getApplicationContext());
+//              String urlFile = uri.getPath();
+//              String extentionFile  = getExtensionByStringHandling(nameFile).get();
+                listFile =new ArrayList<>();
+                for(Uri urlFile : fileChooser){
+                     String nameFile = getFileName(urlFile,getApplicationContext());
+                     String pathFile = urlFile.getPath();
+                     String extentionFile = getExtensionByStringHandling(nameFile).get();
+                     listFile.add(new FileChooserInfo(nameFile,))
+                }
+//              System.out.println("name file "+ nameFile+",extention file : "+extentionFile);
+//
+//              listFile.add(new FileChooserInfo(nameFile,urlFile,nameImg));
+//              fileChooserAdapter = new FileChooserAdapter(this,R.layout.file_chooser_info_item_activity,listFile);
+//              lv.setAdapter(fileChooserAdapter);
+//
+//              Toast.makeText(context, uri.getPath(), Toast.LENGTH_SHORT).show();
 
-              Toast.makeText(context, uri.getPath(), Toast.LENGTH_SHORT).show();
-
-         }
+         } //end if onActiivity
     }
     public void openFileChooser(View view){
          Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -143,9 +125,11 @@ public class HomeActivity extends AppCompatActivity {
 
                  return res;
     }
+    public int getImageIconFile(String urlFile){
 
-    public
 
+        return 0;
 
+    }
 
 }
